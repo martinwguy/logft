a78a72556de4f6e1dbb52345558dcd1e71cb79d6
Martin Guy <martinwguy@gmail.com>
2016-01-30 16:41:00 +0100

    Drop unused variables

--- logft.c
+++ logft.c
@@ -39,31 +39,6 @@ adaptation of dft.c using log channels separated by
 /*   -SSRATE       Sample rate (32K)
 /*   -AWRITEHDR(0)  Write a header on the file if = 1.
 /*   -dropmidchnls  Drop channels between those tuned to notes
-/*   -xXCORRI(0)   Perform cross correlation.
-         -nNHARM(6)     Number of harmonics in xcorr fn.
-         -cCOMBPRINT(0) Print cross correlation function.
-         -qBACKZERO(1)  Cross corr fn = 0 between harmonics if backzero=1.
-                          Otherwise the average of the x corr fn is 0.
-         -gGRAPH(0)     Will print a frame of the x corr fn after each frame
-                          of logft values.
-         -zNRMXCORR(0)  Will normalize the frame of xcorr values so the max
-                          of the xcorr fn = the max of the logft values.
-         -tXCORRPRNT(0) Will print values of the cross correlation function.
-         -wLOGFTVALIN   If logft values prev written to "logftvalin" will
-                          read this so can try peak picker w/o recalculating.
-         -MMAXREQ(0)    Require that logft be at a max to add into cross
-                          correlation.
-         -RNEGVAL       Sets alternate values of harmonic comb = this value
-                           and sets doubcomb = 1 to use double comb code
-         -kPIKPEAK(0)   Will find peak of cross corr fn if pikpeak = 1.
-               -mGRPHPEAK(0)   Will print frame with the channel of the"picked
-                                   peak" = 1.
-               -ePRNTPIK(0)    Will print value of the channel of "picked peak"
-               -yMIDIFILE      Will output a file of shorts of the midivalues
-                                   corresponding to the "picked peak" for
-                                   graphing with ~brown/bin/sndout+
-are just using the 1st value from miditable[lam] so pretty stupid to calculate
-the whole thing
 /***************************************************************************/
 
 #include <stdlib.h>
@@ -96,38 +71,31 @@ int miditable[MAXLAM + 1];
 float   outbuf[MAXCHNLS], *outbufp;     /* frame of out coefs in mag or db */
 char    midifile[20];
 char    hanfile[20] ;
-char    logftvalin[20];
-/* double  hanfilrd[HANMAX]; */
 float   srate = SRATE, midtuncor=1.;
 main(argc,argv)
  int argc;
  char **argv;
 {  /* begin main */
         double  *hanfilrd;
-        double  theta, a, b, c, db, *wsin, *wcos, *hanwindow, one06, one03, y;
-        double  hansum[100], hspacd[21];
-        double  onedws, pidws, twopidws, RES2pidws, freqrat, alpha,phase;
-        int     frmsz = FRMSZ, windbytes, framebytes, ncoefs,nchnls=NCHNLS;
-        int     p, hanning = 1, dbout = 0, print = 0,frmcnt = 0,combprint=0,l;
-        int     windsiz[MAXCHNLS], hspac[41], *hspacptr, xcorri=0,graph=0;
+        double  theta, a, b, c, db, one06, one03;
+        double  onedws, twopidws, RES2pidws, alpha,phase;
+        int     frmsz = FRMSZ, windbytes, framebytes, nchnls=NCHNLS;
+        int     hanning = 1, dbout = 0, print = 0,frmcnt = 0;
+        int     windsiz[MAXCHNLS];
         int     flag, windmaxi, sumwind=0, res = RES, nharm=NHARM;
-        int     points, m, xcorrprnt=0, nw,rempoints, pikpeak=0,hisamp,sampno;
-        int     nextsamp, grphpeak=0, prntpik=0, ofp, ifp;
-        int     minmidi, minlam, line=0, noout=0, nrmxcorr=0, halfsemi=1;
-        int     midifreq, midi[MAXCHNLS], varres=2, flaghi = 1, flagm = 0;
-        int     ofpm, backzero=1, ifp2, fltresp=0, maxreq=0;
-        int     hamming = 1, windsiz_1 = 0,siz, calcphase=0, doubcomb=0;
-        int     rlcmbg, writehdr=0, dropmidchnls=0;
-        short   pitch;
+        int     nw;
+        int     grphpeak=0, prntpik=0;
+        int     minmidi, minlam, line=0, noout=0, halfsemi=1;
+        int     midifreq, midi[MAXCHNLS], varres=2, flaghi = 1;
+        int     maxreq=0;
+        int     hamming = 1, windsiz_1 = 0, calcphase=0;
+        int     writehdr=0, dropmidchnls=0;
         char    *cp;
-        float   windmax, twopi = TWOPI, pi = PI, minhz= MINHZ, tuncorrec=1.00;
-        float   neg, *combptr, comb[MAXCHNLS], xcorr[MAXCHNLS],windsizf[MAXCHNLS];
-        float   dltn, dltnm1, xn, xnm1, *xcorrp, hival,nextval;
-        float   *peakpicp, peakpic[MAXCHNLS], hivaloutbuf, hivalxcorr,normxc;
-        float   negval = -.02;
+        float   windmax, minhz= MINHZ, tuncorrec=1.00;
+        float   windsizf[MAXCHNLS];
         register int    n, k;
         register short  *samp, *samp2;
-        register double *sinp, *cosp, *hanp, *hanfilrdp;
+        register double *hanp, *hanfilrdp;
 
         argc--; argv++;                         /* align onto command args */
         while ((cp = *argv) && *cp++ =='-' && (flag = *cp++))
@@ -149,19 +117,9 @@ main(argc,argv)
                                 break;
                         case 'p':sscanf(cp,"%d",&nchnls);/*num points in ft*/
                                 break;
-                        case 'c': sscanf(cp, "%d", &combprint);
-                                break;
                         case 'n': sscanf(cp, "%d", &nharm);
                                 break;
-                        case 'g': sscanf(cp, "%d", &graph);
-                                break;
-                        case 'x': sscanf(cp, "%d", &xcorri);
-                                break;
-                        case 't': sscanf(cp, "%d", &xcorrprnt);
-                                break;
                         case 'm': sscanf(cp, "%d", &grphpeak);
-                                break;
-                        case 'k': sscanf(cp, "%d", &pikpeak);
                                 break;
                         case 'e': sscanf(cp, "%d", &prntpik);
                                 break;
@@ -169,16 +127,9 @@ main(argc,argv)
                                 break;
                         case 'o': sscanf(cp, "%d", &noout);
                                 break;
-                        case 'z': sscanf(cp, "%d", &nrmxcorr);
-                                break;
                         case 'a': sscanf(cp, "%d", &halfsemi);
                                 break;
                         case 'v': sscanf(cp, "%d", &varres);
-                                break;
-                        case 'y': sscanf(cp, "%s", midifile);
-                                flagm=1;
-                                break;
-                        case 'q': sscanf(cp, "%d", &backzero);
                                 break;
                         case 'W':  sscanf(cp, "%d", &windsiz_1);
                                 break;
@@ -189,9 +140,6 @@ main(argc,argv)
                         case 'S': sscanf(cp, "%f", &srate);
                                 break;
                         case 's': sscanf(cp, "%d", &calcphase);
-                                break;
-                        case 'R': sscanf(cp, "%f", &negval);
-                                doubcomb = 1;
                                 break;
                         case 'Q': sscanf(cp, "%f", &midtuncor);
                                 break;
@@ -220,13 +168,8 @@ main(argc,argv)
         minmidi = miditable[minlam];
         if(nchnls > MAXCHNLS)die("Too many channels");
 
-        if(doubcomb) nharm *= 2;
         minhz /= tuncorrec;
 
-        if (flagm){
-          ofpm = creat(midifile,PMODE);
-          fprintf(stderr, "Prnting midi values to file %s\n", midifile);
-        }
         if (writehdr){
            fprintf(stderr, "Writing a header on outfile\n");
            hdr.frm_magic = FRM_MAGIC;
@@ -249,35 +192,7 @@ main(argc,argv)
                     and equals  srate*res/windsiz[k] */
         if (n= read(0, dumphdr, 1024) != 1024)
           die("something wrong with header dump\n");
-        p = nharm;  l=1;
-        while(p--){ /*calc spacing of harmonics hspac[l] in channels
-                      for nharm harmonic components */
-           if(halfsemi==1)hspacd[l] = log((double)l)/log(one03);
-           else hspacd[l] = log((double)l)/log(one06);
-           hspac[l] = (int)(hspacd[l] + .5);
-           if(combprint)
-             fprintf(stderr,
-               "nharm %d hspacd[%d]= %.2f\tdiff=%.2f (int)hspac= %d\n"
-                ,l,l,hspacd[l],hspacd[l]-hspacd[l-1], hspac[l]);
-           ++l;
-        }
-        hspacptr = hspac; ++hspacptr;  /* harmonic spacing pointer */
-       k = nchnls; l = 0;
-        if (backzero) neg = 0;
-        else
-          neg = -(float)nharm/(nchnls - nharm); /* makes ave of comb 0 */
-        while (k--){
-                   if(doubcomb){
-                      m = hspacptr - hspac;
-                      comb[l] = (l == *hspacptr ? (m % 2 == 0 ? 1. : negval) : neg);
-                   }
-                   else comb[l] = (l == *hspacptr ? 1. : neg); /* comb = 1 at
-                                    a harmonic position and neg elsewhere*/
-                   if ((comb[l]==1. || comb[l]==negval) && l < hspac[nharm]) ++hspacptr;
-                   if (combprint)
-                       fprintf(stderr, "comb[%d] = %.1f\n", l, comb[l]);
-                   ++l;
-        }
+
         k=nchnls;       n=0;
         while (k--){
            midi[n]= minmidi + ((halfsemi==1) ? (n >> 1) : n) ;
@@ -304,11 +219,11 @@ main(argc,argv)
 	/* Calculate window (or rect) values */
 	hanp = hanfilrd;
 	for (k=0; k < nchnls; ++k){
-	    if(windsiz_1)twopidws = twopi/(windsiz[k] - 1);
-	    else twopidws = twopi/(windsiz[k]);
+	    if(windsiz_1)twopidws = TWOPI/(windsiz[k] - 1);
+	    else twopidws = TWOPI/(windsiz[k]);
 	    if(varres && (midi[k]>90))
-		 RES2pidws = (float)varres* res* twopi/windsiz[k];
-	    else RES2pidws = res* twopi/windsiz[k];
+		 RES2pidws = (float)varres* res* TWOPI/windsiz[k];
+	    else RES2pidws = res* TWOPI/windsiz[k];
 	    onedws = 1./windsiz[k];
 
 	    for (n=0; n < windsiz[k]; ++n){
@@ -355,68 +270,6 @@ frame:
 			 k, midifreq, srate*(midi[k]>90?varres*res:res)/windsiz[k], db );
 	   }
 	}                            /* end calc from table read in */
-        if(xcorri){ /* cross correlate   */ /* note must change for one06
-                                     or not use doubcomb */
-
-           points = nchnls - hspac[nharm];/* do xcorrelation on "points"points*/
-                                    /*stop before hspac[] reaches beyond EOF*/
-	  if(doubcomb){ /* do "for ..." for doubcomb case */
-	     rlcmbg = (int) (log((double)2.)/log(one03));
-	     for (k=0; k < points; ++k){   /* begin for of xcorr values */
-		xcorr[k] = 0.;             /* initialize cross corr for kth point */
-		l = k;
-		m = 0;
-		if (k < rlcmbg){
-		   combptr = comb + rlcmbg;
-		   p = nchnls - k;
-		   m = k;
-		   while(p--)
-		     xcorr[k] += outbuf[m++] * (*combptr++);
-		}
-		else {
-		   combptr = comb;
-		   /*zzz*/                p = nchnls - k + rlcmbg;
-		   m = k -rlcmbg;
-		   while(p--)
-		      xcorr[k] += outbuf[m++] * (*combptr++);
-		}
-	     }
-	  } else {
-	     for (k=0; k < points; ++k){
-		xcorr[k] = 0;              /* initialize cross corr for kth point */
-		l = k;
-		m = 0;
-		while(l--)
-		  xcorr[k] += outbuf[m++] * neg; /* mult points before kth
-						    by neg*/
-		p = nchnls - k;            /* p points from k to nchnls */
-		combptr = comb;            /* start comb on the kth point*/
-		while(p--){
-		   if(!maxreq || (*combptr && outbuf[m-1]<outbuf[m] &&
-				  outbuf[m]>outbuf[m+1])){ /* if not requiring max
-					/* or if at a max of logft */
-			  xcorr[k] += outbuf[m++] * (*combptr++);
-
-			}
-			else {  /* max req but no max in fn  or not at the
-				   position of a harmonic */
-			  xcorr[k] += outbuf[m++] * neg;
-			  ++combptr;
-			}
-		      }
-		    } /* end for calc of points values of xcorr[k] */
-	  }/* end else for ordinary comb */
-	  /*both can use from here */
-	  rempoints = nchnls - points;/*remaining points for number of points
-					 in frame = nchnls*/
-	  while(rempoints--)
-		   xcorr[k++] = 0.;
-	  /* to print to stdout */ k = nchnls; l=0;
-	  if(xcorrprnt){
-	    while(k--)
-	      fprintf(stderr,"xcorr[%d]=%.2f\n", l++, xcorr[l]);
-	  }
-       }
 
        if (noout==0){/*if noout==1 don't print to output file; want numbers */
               if((nw = write(1, outbuf, 4*nchnls)) < 0)/*print outbuf to file*/
@@ -428,103 +281,7 @@ frame:
                 and should have written %d\n", nw, 4*nchnls);
        }
 
-        /* if want normalized graph of xcorrelation */
-        if(nrmxcorr && xcorri){
-                 hivaloutbuf = 0.; xn = 0.;
-                 l = nchnls;
-                 outbufp = outbuf;
-                 while(l--){ /*find max of outbuf for current frame */
-                          xnm1 = xn;
-                          xn = *outbufp++;
-                          dltnm1 = dltn;
-                          dltn = xn - xnm1;
-                          if(((dltn*dltnm1)<0) && (dltn < 0)){ /* positive pk*/
-                                if(xnm1 > hivaloutbuf){
-                                         hivaloutbuf = xnm1;
-                                }
-
-                          }
-                 }/* end of calc max of outbuf */
-
-                 hivalxcorr = 0.; xn = 0.;
-                 l = nchnls;
-                 xcorrp = xcorr;
-                 while(l--){ /*find max of xcorr for current frame */
-                          xnm1 = xn;
-                          xn = *xcorrp++;
-                          dltnm1 = dltn;
-                         dltn = xn - xnm1;
-                          if(((dltn*dltnm1)<0) && (dltn < 0)){ /* positive pk*/
-                                if(xnm1 > hivalxcorr){
-                                         hivalxcorr = xnm1;
-                                }
-                          }
-                 }/* end find max for xcorr for this frame */
-                 normxc = hivaloutbuf/hivalxcorr;
-                 l= nchnls; xcorrp = xcorr;
-                 while(l--)/* normalize xcorrp to have same max as outbuf */
-                   *xcorrp = normxc* *xcorrp++;
-       } /* end normalization of xcorr */
-
-       if(xcorri==1 && graph==1){  /*if want graph of cross correlation */
-          if ((nw = write(1, xcorr, 4*nchnls)) < 0){
-            fprintf(stderr, "nw = %d  ERROR writing comb\n", nw);exit(1);
-          }
-          if (nw < 4*nchnls){
-            fprintf(stderr, "error writing from comb\n wrote %d bytes\
-                and should have written %d\n", nw, 4*nchnls); exit(1);
-          }
-       }  /*end if want graph of cross correlation */
        ++frmcnt;
-
-        /* peak picker for cross correlation */
-        if(pikpeak && xcorri){
-                 sampno = 0; hival = 0.; xn = 0.;
-                 l = nchnls;
-                 xcorrp = xcorr;
-                 while(l--){ /*search for peaks in  current frame */
-                          xnm1 = xn;
-                          xn = *xcorrp++;
-                          dltnm1 = dltn;
-                          dltn = xn - xnm1;
-                          if(((dltn*dltnm1)<0) && (dltn < 0)){ /* positive pk*/
-                                if(xnm1 > hival){
-                                         nextval = hival;
-                                         hival = xnm1;
-                                         nextsamp = hisamp;
-                                         hisamp = sampno-1;
-                                }
-                                else if(xnm1 > nextval){
-                                         nextval = xnm1;
-                                         nextsamp = sampno-1;
-                                }
-                          }
-                          ++sampno;
-                 }/* end search for peaks */
-                 if(flagm){ /* print midi value picked to file for graphing */
-                   pitch = (short)(((halfsemi==1) ? (hisamp >> 1) : hisamp)
-                                   + minmidi);
-                   if (dropmidchnls)/* throw out channels half way bet tuned
-                                       notes */
-                     if ((pitch - (short)minmidi) % 2 != 0)
-                       pitch = 0;
-                   write(ofpm, &pitch,2);
-                 }
-
-                 if(prntpik)
-                   fprintf(stderr,"\t%d=midip (%d=chnl max %.0f)\t%d=nextmidip (%d=chnl max %.0f)",((halfsemi==1) ? (hisamp >> 1) : hisamp
-) +minmidi, hisamp,hival,((halfsemi==1) ? (nextsamp >> 1) : nextsamp) +minmidi, nextsamp,
-                           nextval);
-                 if(grphpeak){ /* if want graph of result of peakpicker*/
-                          l = nchnls;
-                          peakpicp = peakpic; n=0;
-                          while(l--){
-                                   if(n++==hisamp)*peakpicp++ = hival;
-                                   else *peakpicp++ = 0. ;
-                          }
-                          write(1, peakpic, 4*nchnls);
-                 }/* end if want graph of result of peakpicker*/
-        } /*end peak picker  */
 
        if ((n = windmaxi - frmsz) > 0) {       /* nxt: for step < windowsiz */
                 samp = sampbuf;
@@ -556,13 +313,12 @@ die(s)
 
 makemiditable() {  /* returns midi value for given period in samples */
          float A, B, freq;
-         int lam, freqi;
+         int lam;
          A = (float)(12.0/log(2.0));
          B = (float)(69.0 - A* log(440.0));
          for (lam = MINLAM; lam <= MAXLAM; ++lam) {
 /*                freq = (srate/lam)*1.04;    /* 4% flat tuning  */
                   freq = (srate/lam)*midtuncor;
                   miditable[lam] = (int)(A * log(freq) + B + 0.5);
-                  freqi = (int)freq;
          }
 }
